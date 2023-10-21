d2a84f7a47f06322fcff385e848c9b18c1af4d0d
PyProdger <deeptimaankrishnajadaun@gmail.com>
2023-10-21 16:08:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,61 +1,64 @@
 require('dotenv').config();
 
-const express = require('express')
-const mongoose = require('mongoose')
-const doctor = require("./models/Doctor.model");
+const express = require('express');
+const mongoose = require('mongoose');
+const Doctor = require('./models/Doctor.model');
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-mongoose.set('strictQuery', false);
+mongoose.set('strict', false);
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log(error);
+    console.error(error);
     process.exit(1);
   }
-}
+};
 
-//Routes go here
-app.get('/', (req,res) => {
-    res.send({ title: 'Doctor' });
-})
-
-app.get('/doctorLogin', async (req,res)=> {
-
-  const doctor = await doctor.find();
-  if (doctor) {
-    res.json(doctor)
-  } else {
-    res.send("Something went wrong.");
-  }
-  
+app.get('/', (req, res) => {
+  res.json({ title: 'Doctor' });
 });
 
-app.get('/add-doctor', async (req,res) => {
+app.get('/doctors', async (req, res) => {
   try {
-    await doctor.insertMany([
-      {
-      userType: "Hospital",
-      docID: "4452",
-      password: "password123",
-    },
-    {
-      userType: "Hospital",
-      docID: "2522",
-      password: "123",
-    }
-    ]);
-    res.json({"Data":"Added"})
+    const doctors = await Doctor.find();
+    res.json(doctors);
   } catch (error) {
-    console.log("err", + error);
+    console.error(error);
+    res.status(500).json({ error: 'Something went wrong' });
   }
-})
+});
+
+app.get('/add-doctor', async (req, res) => {
+  try {
+    const result = await Doctor.insertMany([
+      {
+        userType: 'Hospital',
+        docID: '4452',
+        password: 'password123',
+      },
+      {
+        userType: 'Hospital',
+        docID: '2522',
+        password: '123',
+      }
+    ]);
+    res.json({ message: 'Data Added', result });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
 
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log("listening for requests");
-    })
-})
+  app.listen(PORT, () => {
+    console.log('Listening for requests');
+  });
+});
