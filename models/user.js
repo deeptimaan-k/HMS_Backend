9b8b10cf597fb79ecd9b9032d1139aa3ff63ab05
PyProdger <deeptimaankrishnajadaun@gmail.com>
2023-10-21 17:05:34 +0530

    new

--- models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
-  userType: String, // "Hospital," "Patient," "Insurance," etc.
-  userID: int, // You can use a different data type as needed
-  password: String, // Store hashed passwords
-});
-
-module.exports = mongoose.model('User', userSchema);
