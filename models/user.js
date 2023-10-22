7ae92a4dae43e687e63923113e241095b2e1313a
PyProdger <deeptimaankrishnajadaun@gmail.com>
2023-10-22 21:28:52 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -4,6 +4,20 @@ const userSchema = new mongoose.Schema({
   userType: String,
   userID: String,
   password: String,
+  patientName: String,
+  mobile: String,
+  email: String,
+  age: Number,
+  department: String,
+  gender: String,
+  bloodGroup: String,
+  DOB: String,
+  address: String,
+  image: String,
+  disease: String,
+  details: String,
+  admitted: Boolean,
+  date: Date,
 });
 
 const User = mongoose.model("User", userSchema);
