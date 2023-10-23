e290694123527bb01b231f580156af79d6ce12ce
PyProdger <deeptimaankrishnajadaun@gmail.com>
2023-10-23 15:23:07 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-require('dotenv').config(); // Load environment variables
+const dotenv = require('dotenv');
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
