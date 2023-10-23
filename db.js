a6b13ea946fa3bfb8d5b960751b1d417292af31e
PyProdger <deeptimaankrishnajadaun@gmail.com>
2023-10-23 15:30:32 +0530

    now

--- db.js
+++ db.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
